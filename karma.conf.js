8ccde1b61a383195e2b3f085c1c93176148ded45
vishnurathina <vishnupriya.rathina@aspiresys.com>
2022-09-25 15:45:20 +0530

    added sonar qube config

--- karma.conf.js
+++ karma.conf.js
@@ -11,6 +11,7 @@ module.exports = function (config) {
       require("karma-jasmine-html-reporter"),
       require("karma-coverage"),
       require("@angular-devkit/build-angular/plugins/karma"),
+      require('karma-sonarqube-reporter')
     ],
     client: {
       jasmine: {
@@ -21,6 +22,25 @@ module.exports = function (config) {
       },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
+    sonarqubeReporter: {
+      basePath: 'src/app', // test files folder
+      filePattern: '**/*spec.ts', // test files glob pattern
+      encoding: 'utf-8', // test files encoding
+      outputFolder: 'reports', // report destination
+      legacyMode: false, // report for Sonarqube < 6.2 (disabled)
+      reportName: function (metadata) {
+        // report name callback, but accepts also a
+        // string (file name) to generate a single file
+        /**
+         * Report metadata array:
+         * - metadata[0] = browser name
+         * - metadata[1] = browser version
+         * - metadata[2] = plataform name
+         * - metadata[3] = plataform version
+         */
+        return 'sonarqube_report.xml';
+      },
+    },
     jasmineHtmlReporter: {
       suppressAll: true, // removes the duplicated traces
     },
@@ -29,7 +49,7 @@ module.exports = function (config) {
       subdir: ".",
       reporters: [{ type: "html" }, { type: "text-summary" }],
     },
-    reporters: ["progress", "kjhtml"],
+    reporters: ["progress", "kjhtml", "sonarqube"],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { ReactiveFormsModule ,FormsModule ,FormBuilder} from '@angular/forms';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,6 +9,8 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule,ReactiveFormsModule,FormsModule],
+      providers: [FormBuilder],
       declarations: [LoginComponent],
     }).compileComponents();
 
